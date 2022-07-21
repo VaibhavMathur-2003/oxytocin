56a24bb12c611d6c84f800dd287200168d856ba7
VaibhavMathur-2003 <mathurvaibhav010@gmail.com>
2022-07-21 21:49:09 +0530

    modified

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Note.css";
 
 function Note(props) {
-
-  const updateText = (text, id) => {props.updateText(text, id)}
-  const category = (cate , id) => {props.category(cate, id)}
+  const [color, setColor] = useState("")
+  useEffect(()=>setColor(Math.random().toString(16).substr(-6)),[])
+  const updateText = (text, id) => {
+    props.updateText(text, id);
+  };
+  const category = (cate, id) => {
+    props.category(cate, id);
+  };
   return (
-    <div className="notes">
+    <div className="notes" style={{backgroundColor:"#"+color}}>
       <textarea
         className="text"
         defaultValue={props.note.text}
         onChange={(event) => updateText(event.target.value, props.note.id)}
       />
-      <p>Enter category</p>
-      <textarea type="text" onChange={(e) => category(e.target.value, props.note.id)} defaultValue="none"/>
+      {!props.filter && (
+        <input
+          className="category"
+          type="text"
+          onChange={(e) => category(e.target.value, props.note.id)}
+          defaultValue="Enter category"
+        />
+      )}
       <div className="footer">
-        <button onClick={() => props.deleteNote(props.note.id)}>Delete</button>
+        <div>
+        <button
+          style={{
+            backgroundColor: "white",
+            fontSize: ".7rem",
+            color: "red",
+            borderRadius: "10px",
+            marginTop: "10px",
+          }}
+          onClick={() => props.deleteNote(props.note.id)}
+        >
+          🗑Del
+        </button>
+        </div>
+        <div   className="showCategory">
+        <input
+        style={{objectFit:"contain"}}
+        defaultValue={props.note.cate}
+        disabled
+      />
+      </div>
       </div>
     </div>
   );
--- src/Components/NotePage.js
+++ src/Components/NotePage.js
@@ -3,8 +3,8 @@ import Note from "./Note";
 import "./NotePage.css";
 
 function NoteContainer(props) {
-  const [filtered, setFiltered] = useState(false)
-  const [fill, setFill] = useState("none")
+  const [filtered, setFiltered] = useState(false);
+  const [fill, setFill] = useState("none");
   const notesArray = (arr) => {
     const array = [];
     for (let i = arr.length - 1; i >= 0; --i) {
@@ -17,38 +17,74 @@ function NoteContainer(props) {
 
   return (
     <div className="page">
-      <button onClick={() => props.addNote()}> Add Note + </button>
+      <div className="title">Messaging</div>
       <div>
-        <h2 onClick={()=>setFiltered(!filtered)}>Filter(Click here to activate filter)</h2>
-        {filtered && <select onChange={(e)=>{setFill(e.target.value)}}>
-          <option>none</option>
-          <option>Good Mood</option>
-          <option>Pain</option>
-          <option>Career</option>
-          <option>Family</option>
-        </select>}
+        <div className="add">
+          <div>
+            <div onClick={() => setFiltered(!filtered)}>
+              <span>Filter :  {filtered ? "ON":"OFF"}</span>
+            </div>
+            {filtered && (
+              <select
+                onChange={(e) => {
+                  setFill(e.target.value);
+                }}
+              >
+                <option>none</option>
+                <option>Good Mood</option>
+                <option>Pain</option>
+                <option>Career</option>
+                <option>Family</option>
+              </select>
+            )}
+          </div>
+          <div>
+          <div className="empty">
+        {(notes.length<1)? "Add a note": ""}
+        </div>
+            {<button
+              style={{
+                backgroundColor: "black",
+                fontSize: "1.3rem",
+                color: "white",
+              }}
+              onClick={() => {props.addNote(); setFiltered(false)}} 
+            >
+              {" "}
+              🗏{" "}
+            </button>}
+          </div>
+        </div>
+        <div className="filterTitle"> {filtered && <h2>{fill}</h2>}</div>
       </div>
+
       <div className="pageNotes">
-      {!filtered && notes.map((item) => (
-          <Note
-            key={item.id}
-            note={item}
-            deleteNote={props.deleteNote}
-            updateText={props.updateText}
-            category = {props.category}
-          />
+        
+        {!filtered &&
+          notes.map((item) => (
+            <Note
+              key={item.id}
+              note={item}
+              deleteNote={props.deleteNote}
+              updateText={props.updateText}
+              category={props.category}
+            />
           ))}
-        {filtered && notes.filter((item) => (item.cate.toLowerCase() === fill.toLowerCase())).map((item) => (
-          <Note
-            key={item.id}
-            note={item}
-            deleteNote={props.deleteNote}
-            updateText={props.updateText}
-            category = {props.category}
-          />
-          ))}
+
+        {filtered &&
+          notes
+            .filter((item) => item.cate.toLowerCase() === fill.toLowerCase())
+            .map((item) => (
+              <Note
+                key={item.id}
+                note={item}
+                deleteNote={props.deleteNote}
+                updateText={props.updateText}
+                category={props.category}
+                filter = {filtered}
+              />
+            ))}
       </div>
-      
     </div>
   );
 }
